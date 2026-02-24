a21c832882388abff842b819d8737a2d2394099a
Arnold-Ka <arnoldkarambiri293@gmail.com>
2026-02-24 17:12:31 +0000

    ajout du listening

--- src/main/java/com/hackers/freelancia/FreelanciaApplication.java
+++ src/main/java/com/hackers/freelancia/FreelanciaApplication.java
@@ -2,6 +2,7 @@ package com.hackers.freelancia;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 /**
  * class général.
@@ -11,6 +12,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * Copyright (c) 2021 All rights reserved.
  * @since : 19/01/2026 à 13:51
  */
+@EnableJpaAuditing
 @SpringBootApplication
 public class FreelanciaApplication {
 
